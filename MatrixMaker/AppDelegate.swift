a021b4c32c0746b5e81fc4722881720bf5e5ebb3
Justin England <justin@rapidcity.net>
2015-06-29 21:35:04 -0700

    Initial Commit

---
+++ MatrixMaker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MatrixMaker
+//
+//  Created by Justin England on 6/29/15.
+//  Copyright (c) 2015 Justin England. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
